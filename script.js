7b94d3a4f5c03caf23a97bd2f67471118444aff9
Aishwarya-Kurdekar <122003337+Aishwarya-Kurdekar@users.noreply.github.com>
2024-11-03 19:45:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let profileMenu = document.getElementById("profileMenu");
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+}
+function toggleActivity(){
+    sidebarActivity.classList.toggle("open-activity");
+}
